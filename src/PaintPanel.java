c5f00225c6bf0caf9cff163225a24d3495c595eb
league <league@wts5.attlocal.net>
2019-02-11 18:27:38 -0800

    ALVNCZSCVFDH

--- src/PaintPanel.java
+++ src/PaintPanel.java
@@ -1,33 +1,72 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class PaintPanel extends JPanel implements KeyListener{
+public class PaintPanel extends JPanel implements KeyListener, ActionListener{
 	
+	Timer refresh;
+	//Player player = new Player(0, 0, );
 	int gameState = 0;
 	private Color menuColor = Color.BLUE;
 	private Color gameColor = Color.BLACK;
 	private Color endColor = Color.RED;
-	public void paintComponent(Graphics g) {
-		if (gameState == 0) {
-			g.setColor(menuColor);
-			g.fillRect( 0, 0, 1000, 1000);
-		} else
+	
+	PaintPanel (){
+		refresh = new Timer(1000 / 60, this);
+		refresh.start();
+	}
 
-		if (gameState == 1) {
-			g.setColor(gameColor);
-			g.fillRect( 0, 0, 1000, 1000);
-		} else
+		public void paintComponent(Graphics g) {
+				if (gameState == 0) {
+					g.setColor(menuColor);
+					g.fillRect( 0, 0, 1000, 1000);
+				} else
 		
-		if (gameState == 2) {
-			g.setColor(endColor);
-			g.fillRect( 0, 0, 1000, 1000);
+				if (gameState == 1) {
+					g.setColor(gameColor);
+					g.fillRect( 0, 0, 1000, 1000);
+				} else
+				
+				if (gameState == 2) {
+					g.setColor(endColor);
+					g.fillRect( 0, 0, 1000, 1000);
+				}
 		}
+		
+		
+//																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																			
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (gameState > 3) {
+				gameState = 1;
+			} else {
+				gameState += 1;
+			}
+		}
+		
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	// Totally Important stuff
 	public int getGameState() {
 		return gameState;
 	}
@@ -48,11 +87,5 @@ public class PaintPanel extends JPanel implements KeyListener{
 		
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-			
-		}
-		
-	}
+
 }
